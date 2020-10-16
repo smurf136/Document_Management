6347bdb69ed452deb27b1b61e9e82ef676deacfe
Smurf <smurf@Smurfs-MacBook-Pro-2.local>
2020-10-16 15:21:56 +0700

    [Update] update dependecies add lombok and fixed red code

---
+++ src/main/java/controller/ProtocolController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProtolcolController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> hola(){
+        return new ResponseEntity<String>("hello", HttpStatus.OK);
+    }
+
+}
